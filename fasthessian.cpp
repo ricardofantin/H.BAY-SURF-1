6434885f28a40e6e7ad144b3e10448275b158b8d
Ricardo Fantin da Costa <ricardofantin@gmail.com>
2018-01-31 18:15:37 -0200

    Filtros bidimensionais implementados e projeto gera dois executáveis

--- fasthessian.cpp
+++ fasthessian.cpp
@@ -3,7 +3,7 @@
 using namespace surf;
 
 //! Destructor
-//FastHessian::~FastHessian();
+FastHessian::~FastHessian(){};
 
 //! Constructor with parameters
 FastHessian::FastHessian(Image *im, std::vector<Ipoint> ip, double thres, bool doub, 
@@ -61,12 +61,30 @@ void FastHessian::getInterestPoints(){
 void FastHessian::calculateIpoint(int r, int c, Ipoint *ip){
 	Image *i = _Iimage;
 	double x, y, xy;
-	y = i->getRectangleSum(r-4, c-2, 3, 5) -2*i->getRectangleSum(r-1, c-2, 3, 5) + i->getRectangleSum(r+2, c-2, 3, 5);
 	x = i->getRectangleSum(r-2, c-4, 5, 3) -2*i->getRectangleSum(r-2, c-1, 5, 3) + i->getRectangleSum(r-2, c+2, 5, 3);
+	y = i->getRectangleSum(r-4, c-2, 3, 5) -2*i->getRectangleSum(r-1, c-2, 3, 5) + i->getRectangleSum(r+2, c-2, 3, 5);
 	xy = i->getRectangleSum(r-3, c-3, 3, 3) + i->getRectangleSum(r+1, c+1, 3, 3) - i->getRectangleSum(r-3, c+1, 3, 3) - i->getRectangleSum(r+1, c-3, 3, 3);
 	ip->ivec[0] = x;
 	ip->ivec[1] = y;
 	ip->ivec[2] = xy;
+	x = i->getRectangleSum(r-4, c-7, 9, 5) -2*i->getRectangleSum(r-4, c-2, 9, 5) + i->getRectangleSum(r-4, c+3, 9, 5);
+	y = i->getRectangleSum(r-7, c-4, 5, 9) -2*i->getRectangleSum(r-2, c-4, 5, 9) + i->getRectangleSum(r+3, c-4, 5, 9);
+	xy = i->getRectangleSum(r-6, c-6, 6, 6) + i->getRectangleSum(r+1, c+1, 6, 6) - i->getRectangleSum(r-6, c+1, 6, 6) - i->getRectangleSum(r+1, c-6, 6, 6);
+	ip->ivec[3] = x;
+	ip->ivec[4] = y;
+	ip->ivec[5] = xy;
+	x = i->getRectangleSum(r-6, c-10, 13, 7) -2*i->getRectangleSum(r-6, c-3, 7, 13) + i->getRectangleSum(r-6, c+4, 7, 13);
+	y = i->getRectangleSum(r-10, c-6, 7, 13) -2*i->getRectangleSum(r-3, c-6, 7, 13) + i->getRectangleSum(r+4, c-6, 7, 13);
+	xy = i->getRectangleSum(r-9, c-9, 9, 9) + i->getRectangleSum(r+1, c+1, 9, 9) - i->getRectangleSum(r-9, c+1, 9, 9) - i->getRectangleSum(r+1, c-9, 9, 9);
+	ip->ivec[6] = x;
+	ip->ivec[7] = y;
+	ip->ivec[8] = xy;
+	x = i->getRectangleSum(r-8, c-13, 17, 9) -2*i->getRectangleSum(r-8, c-4, 17, 9) + i->getRectangleSum(r-8, c+5, 17, 9);
+	y = i->getRectangleSum(r-13, c-8, 9, 17) -2*i->getRectangleSum(r-4, c-8, 9, 17) + i->getRectangleSum(r+5, c-8, 9, 17);
+	xy = i->getRectangleSum(r-12, c-12, 12, 12) + i->getRectangleSum(r+1, c+1, 12, 12) - i->getRectangleSum(r-12, c+1, 12, 12) - i->getRectangleSum(r+1, c-12, 12, 12);
+	ip->ivec[9] = x;
+	ip->ivec[10] = y;
+	ip->ivec[11] = xy;
 }
 
     //! Create a new ipoint at location (x, y),  at a certain scale 
